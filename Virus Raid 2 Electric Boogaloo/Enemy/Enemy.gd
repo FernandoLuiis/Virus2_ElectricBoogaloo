6dc16c4f35e0fc22c816e04db621ee27d6145415
Bruno Ceresér <bruno.cereser@catolicasc.edu.br>
2023-03-27 18:56:43 -0300

    Adjust to Enemy Death Counter

--- Virus Raid 2 Electric Boogaloo/Enemy/Enemy.gd
+++ Virus Raid 2 Electric Boogaloo/Enemy/Enemy.gd
@@ -3,8 +3,11 @@ extends Node2D
 var fagocito = preload("res://Enemy/Fagocito/Fagocito.tscn")
 
 func _ready():
-	
+	get_parent().enemy_count += 1
 	Global.instance_node(enemy_type_selector(), global_position, self)
+
+func _exit_tree():
+	get_parent().enemy_count -= 1
 
 func follow_player(speed, delta):
 	if Global.player != null:
